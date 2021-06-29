ba1cf74667f3e8b0c3010e66f2484162737b8b33
v.monasteretskyy <v.monasteretskyy@gmail.com>
2021-06-29 14:49:25 +0300

    update

--- js/common.js
+++ js/common.js
@@ -243,20 +243,88 @@ $(document).ready(function () {
         $('.overflowhiddsect').toggleClass('h-auto');
     });
 
+    // main page - header scroll links
 
+    // header__mainpage
 
+    $(".header__mainpage").on("click", ".scrollpage a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+    });
 
+    $(".mobmenulist").on("click", ".scrollpage a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+    });
 
+    $('.mobmenulist li.scrollpage').click(function () {
+        $('.mobile-menu').removeClass('show');
+    });
 
+    // Timer
+    if ($('body *').is('#clockdiv')) {
+        function getTimeRemaining(endtime) {
+            var t = Date.parse(endtime) - Date.parse(new Date());
+            var seconds = Math.floor((t / 1000) % 60);
+            var minutes = Math.floor((t / 1000 / 60) % 60);
+            var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            var days = Math.floor((t / (1000 * 60 * 60 * 24)) % 7);
+            var weeks = Math.floor(t / (1000 * 60 * 60 * 24 * 7));
+            return {
+                'total': t,
+                'weeks': weeks,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
 
+        function initializeClock(id, endtime) {
+            var clock = document.getElementById(id);
+            var weeksSpan = clock.querySelector('.weeks');
+            var daysSpan = clock.querySelector('.days');
+            var hoursSpan = clock.querySelector('.hours');
+            var minutesSpan = clock.querySelector('.minutes');
+            var secondsSpan = clock.querySelector('.seconds');
 
+            function updateClock() {
+                var t = getTimeRemaining(endtime);
+                weeksSpan.innerHTML = t.weeks;
+                daysSpan.innerHTML = t.days;
+                hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
+                if (t.total <= 0) {
+                    clearInterval(timeinterval);
+                }
+            }
 
+            updateClock();
+            var timeinterval = setInterval(updateClock, 1000);
+        }
 
+        var deadline = new Date(Date.parse(new Date()) + 8 * 24 * 60 * 60 * 1000);
+        initializeClock('clockdiv', deadline);
+    }
 
+    // input mask
 
+    if ($('body *').is('.numinp')) {
+        // $('.numinp').inputmask("999999999");
 
-
+        $('.numinp').inputmask({
+            mask: '999999999999',
+            placeholder: ' ',
+            showMaskOnHover: false,
+            showMaskOnFocus: false,
+        });
+    }
 
 
 
